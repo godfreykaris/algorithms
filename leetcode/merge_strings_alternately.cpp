7fcbee26ee16c9f71d8a972c82045e5890d17a33
Karis <godfreykarisw@gmail.com>
2025-04-28 08:34:27 +0300

    LeetCode 1768 Testing edge cases

--- leetcode/merge_strings_alternately.cpp
+++ leetcode/merge_strings_alternately.cpp
@@ -7,6 +7,7 @@ Return the merged string.
 ***********************************************/
 #include <string>
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 class Solution {
@@ -16,32 +17,24 @@ class Solution {
 
 string Solution::mergeAlternately(string word1, string word2) {
     int count1 = 0, count2 = 0;
-
-    int end = word1.length() + 2;
-    
+    int end = word1.length() < word2.length() ? word1.length() : word2.length();
     for(count1 = 0; count1 <= end;) {
-        if(word2[count1])
-        {
-            auto it = word1.insert(word1.begin() + count1 + 1, word2[count2]);
-            count1+=2;
-            count2+=1;
-        }
-        else {
-            count1+=1;
-            end++;
-        }
+        auto it = word1.insert(word1.begin() + count1 + 1, word2[count2]);
+        count1+=2;
+        count2+=1;
         
     }
     
-    if (count2 < word2.length())
-            word1 += word2.substr(count2);
+    if (count2 < word2.length()) {
+        word1 += word2.substr(count2);
+    }
 
     return word1;
 }
 
 int main() {
     Solution sol;
-    string result = sol.mergeAlternately("abcd", "pq");
+    string result = sol.mergeAlternately("trivialcases", "simplestitem");
     cout<< result << endl;
 
     return 0;
